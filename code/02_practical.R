12c01ad1b69225f8b56dd9264e02f94f64f28617
Kiran Dhanjal-Adams <kiran.dhanjaladams@uqconnect.edu.au>
2025-12-03 12:30:48 +0000

    updated R code with Rmd info

--- code/02_practical.R
+++ code/02_practical.R
@@ -35,6 +35,17 @@ library(terra)
 ## Load species distributions
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+# We are going to be using Species Distribution Modelling (SDM) data from students last year. 
+# We are going to use the binary presence/absence outputs where the probability of a species presence 
+# has been translated into a yes/no.
+# 
+# Each layer describes the spatial distribution of a feature. 
+# Here, our feature data correspond to different plant species. 
+# Specifically, if the cell (or planning unit) has a 1, a species is predicted to be present in it based on the SDM. 
+# If the cell has a 0, then it is predicted to be absent.
+
+
+
 # Open file
 species = terra::rast("data/species.tif")
 
@@ -135,17 +146,21 @@ library(highs)
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ## Formulate the problem
 
-# I want to protect as many species as possible within my budget 
-# by setting cells aside for protection, 
+# I want to protect as many species as possible within my budget by setting cells aside for protection, 
 # aiming for 30% of the range of each species
-
-# Translated to code:
-# - We start by creating a new `problem()` that will use a
-# - minimum shortfall objective to spend the budget (via `add_min_shortfall_objective()`), while getting as close as possible with 
-# - relative targets of 30% of the range of each species(via `add_relative_targets()`), 
-# - binary decisions to purchase+protect or not (via `add_binary_decisions()`), and 
-# - near-optimal solutions (i.e., 10% from optimality) using the 
-# - best solver installed on our computer (via `add_default_solver()`).
+# 
+# The things that we need to track are:
+# •	Budget: the total cost of the prioritization will represent a 30% of the total land value in the study area. Given this budget, we want the prioritization to increase feature representation, as much as possible,
+# •	Features: each feature (i.e. species) would, ideally, have 30% of its distribution covered by the prioritization. In this scenario, we can either purchase all of the land inside a given planning unit, or none of the land inside a given planning unit.
+# •	Planning Units: These are sites that we are considering to protect or not. We have the option to protect or not each site. In this case, each site represents one pixel of the raster
+# 
+# We then need to translate this to code:
+# •	We start by creating a new problem() that will use a
+# •	minimum shortfall objective to spend the budget (via add_min_shortfall_objective()), while getting as close as possible to a
+# •	relative targets of 30% of the range of each species(via add_relative_targets()),
+# •	binary decisions to purchase+protect or not (via add_binary_decisions()), and
+# •	near-optimal solutions (i.e., 10% from optimality) using the
+# •	best solver installed on our computer (via add_default_solver()).
 
 
 
@@ -226,12 +241,18 @@ print(pt_target_coverage)
 print(mean(pt_target_coverage$met) * 100)
 
 
-
+# Note that a lot of species still have 70% of their range protected… 
+# How might this be possible when we are only protecting what we can purchase with 30% of our budget 
+# (which is close to 30% of the area of Madagascar)? 
+# This is because many species will have a range that is smaller than ~30% of Madagascar, 
+# so protecting ~30% will protect >30% of many species ranges, especially if they are narrow range species.
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Adding existing PAs
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Madagascar already has a Protected Area network.
+
+# Madagascar already had a Protected Area network. 
+# Do the areas we are protecting overlap with the existing protected areas? 
 # Perhaps we should develop protected areas that complement and enhance the existing ones.
 
 # load protected areas
@@ -246,6 +267,8 @@ plot(PAs, main="High Res PAs")
 PAs = terra::aggregate(PAs, fact = 12, fun = "modal",  na.rm=TRUE)
 plot(PAs, main="Low Res PAs")
 par(mfrow = c(1,1))
+
+# Notice that some PAs get swallowed up by the resolution change.
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ## Modify the problem to add PAs
@@ -273,13 +296,16 @@ eval_n_summary(p2, s2)
 
 # calculate the total cost of the solution
 eval_cost_summary(p2, s2)
-# Note that the scenario is more expensive - why do you think that is?
+
+# Note that the scenario 2 is more expensive - why do you think that is?
+
 # It is likely the first scenario has no constraints and protects the cheapest sites first 
 # that meet the biodiversity target. However, some of these sites are already protected in scenario 2, 
-# so the algorithm has to consider sites that are not already protected, which are more expensive
-# in order to meet the conservation scenario.
-# The more we protect, the more expensive it gets because we use the cheaper sites at the
-# beginning, and add more and more costly sites as we go on
+# so the algorithm has to consider other sites to meet the conservation target. 
+# The current PA system may also be sub optimal (for instance, protecting some ecosystems/species more than others), 
+# so the algorithm has to compensate by protecting more expensive sites 
+# to meet the same biodiversity targets for under-represented ecosystems/species.
+
 
 # calculate target coverage for the solution
 p2_target_coverage <- eval_target_coverage_summary(p2, s2)
@@ -293,9 +319,9 @@ print(mean(p2_target_coverage$met) * 100)
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 # We can further modify the problem by adding penalties that punish overly fragmented solutions 
-# (via `add_boundary_penalties()`). Here we will use a penalty factor 
-# (i.e., boundary length modifier) of 0.003, and an edge factor of 50% so that planning units 
-# that occur on the outer edge of the study area are not overly penalized.
+# (via add_boundary_penalties()). Here we will use a penalty factor (i.e., boundary length modifier) of 0.003,
+# and an edge factor of 50% so that planning units that occur on the outer edge of the study area 
+# are not overly penalized.
 
 
 # create a new problem with boundary penalties added to it
@@ -321,8 +347,16 @@ print(p3_target_coverage)
 print(mean(p3_target_coverage$met) * 100)
 
 
-# What is the first thing you notice? It's much longer... 
-# This is because it has to compute a lot more relationships between cells/planning units.
+# What is the first thing you notice? 
+# It’s much longer… This is because it has to compute a lot more relationships 
+# between cells/planning units.
+# 
+# What is the second thing you notice? 
+# The protected areas are huge. 
+# It’s important to consider whether very large protected areas make sense in human landscapes, 
+# as they could impact livelihoods and displace people. However, large protected areas can also be easier 
+# to manage and patrol, and can protect wide-ranging species better. 
+# It’s important to think about these things when designing protected area networks.
 
 
 
@@ -330,12 +364,12 @@ print(mean(p3_target_coverage$met) * 100)
 # Calculate importance scores
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-# Irreplaceability tells you how important each cell is to the solution:
-# if it is red, you absolutely need to protect that cell. If it is beige, 
-# it is interchangeable with other cells. If you have a lot of common species (like we do), 
-# then most of them will be beige, but if you have rare narrow range endemics, 
-# then those cells will become red (because they only occur in a few locations
-# where they must be protected otherwise they will be lost).
+# Irreplaceability tells you how important each cell is to the solution: 
+# if it is red, you absolutely need to protect that cell. 
+# If it is beige, it is interchangeable with other cells. 
+# If you have a lot of common species (like we do), then most of them will be beige, 
+# but if you have rare narrow range endemics, then those cells will become red 
+# (because they only occur in a few locations where they must be protected otherwise they will be lost).
 
 
 rc <-
@@ -355,12 +389,14 @@ plot(
 # Exercise 3: Lock out some areas
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-# You will "lock out" some areas from protection. 
-# These are places like cities that should be ignored from the prioritisation. 
-# 
-# Go find the `locked_out.tif` raster in your data directory and use `terra::rast` 
-# to import it. Then use the `add_locked_out_constraints(locked_out)` to lock out your raster 
-# from the p2 problem we formulated. If you want you can also plot the locked_out data layer.
+# As an exercise, you will “lock out” some areas from protection. 
+# These are places like cities that should be ignored from the prioritisation.
+
+# Go find the locked_out.tif raster in your data directory 
+# and use terra::rast to import it. Then use the add_locked_out_constraints() 
+# to lock out your raster from the p2 problem we formulated. 
+# If you want you can also plot the locked_out data layer
+
 
 
 locked_out = terra::rast("data/locked_out.tif")
@@ -393,16 +429,22 @@ print(p4_target_coverage)
 print(mean(p4_target_coverage$met) * 100)
 
 
-# Notice that it reality it doesn't change much because we are using the same data to 
-# derive the cost layer and the locked out data
+# Notice that in reality it doesn’t change much because we are using the same data 
+# to derive the cost layer and the locked out layer. However locking areas out of the prioritisation 
+# means that some “expensive” but biodiverse sites cannot be protected, and need to be replaced 
+# with many cheaper planning units, meaning that more land has to be protected to compensate.
 
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Exercise 4: MARXAN
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-# Get the cheapest solution that meets the biodiversity target, this can be done by replacing 
-# add_min_shortfall_objective(budget) with add_min_set_objective() in p1. We no longer rely on a budget
+# Marxan addresses conservation problems the opposite way round to prioritizr. 
+# It tries to find the cheapest solution (that will impact the least people) while meeting the biodiversity target 
+# (here to protect 30% of the range of each species). 
+# This can be done by replacing add_min_shortfall_objective(budget) 
+# with add_min_set_objective() in the p1 solution. 
+# We no longer rely on a budget.
 
 py <-
   problem(cost, features = species) %>%
@@ -426,13 +468,15 @@ par(mfrow=c(1,1))# go back to only 1 plot
 # Exercise 5: Ignore cost
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-# So far we have used cost as the human footprint layer, but what if we want to just ignore cost 
-# and look only at the biodiversity? We are going to create a new cost layer called `cost_null` 
-# where cost is set to 1 on land, and NA everywhere else. 
-# We will then  replace `cost` with `cost_null` in` problem(cost, features = species)`. 
+# So far we have used cost as the human footprint layer, 
+# but what if we want to just ignore cost and look only at the biodiversity? 
+# We are going to create a new cost layer called cost_null where cost is set to 1 on land, 
+# and NA everywhere else. 
+# We will then replace cost with cost_null in problem(cost, features = species).
 # How does it compare to the layer with cost?
+   
+# Hint: replace all cost with cost_null in budget and pr
 
-# Hint: replace all `cost` with `cost_null` in `budget` and `pr`
 
 
 
